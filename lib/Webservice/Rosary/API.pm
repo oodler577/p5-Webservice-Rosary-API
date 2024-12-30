783663336df1dbdb2e11f654a46d79ce4d01971c
O. Odler, 577 <oodler@cpan.org>
2024-12-30 21:04:41 +0000

    initial import

---
+++ lib/Webservice/Rosary/API.pm
@@ -0,0 +1,35 @@
+package Webservice::Rosary::API;
+
+use v5.10;
+use strict;
+
+use Util::H2O::More qw/baptise h2o/;
+use HTTP::Tiny qw//;
+
+our $VERSION = "0.1.2";
+
+sub new {
+
+}
+
+123
+
+__END__
+
+=head1 NAME
+
+Webservice::Rosary::API - Perl API client for the Rosary API at L<https://therosaryapi.cf>.
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+=head1 C<avemaria> UTILITY
+
+=head1 ENVIRONMENT
+
+=head1 BUGS
+
+=head1 LICENSE AND COPYRIGHT
---
+++ t/01-basic.t
@@ -0,0 +1,7 @@
+use strict;
+use warnings;
+use Test::More;
+
+use_ok "Webservice::Rosary::API";
+
+done_testing;
