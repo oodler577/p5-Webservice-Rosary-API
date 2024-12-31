3665091ff3af32e93c993f1806d1fc2b89b9ef04
O. Odler, 577 <oodler@cpan.org>
2024-12-31 04:42:28 +0000

    more progress

--- lib/Webservice/Rosary/API.pm
+++ lib/Webservice/Rosary/API.pm
@@ -3,13 +3,53 @@ package Webservice::Rosary::API;
 use v5.10;
 use strict;
 
-use Util::H2O::More qw/baptise h2o/;
+use Util::H2O::More qw/baptise ddd HTTPTiny2h2o h2o/;
 use HTTP::Tiny qw//;
 
 our $VERSION = "0.1.2";
 
-sub new {
+use constant {
+  BASEURL  => "https://the-rosary-api.vercel.app/v1",
+  DAILYURL => "https://dailyrosary.cf",
+};
 
+sub new {
+  my $pkg = shift;
+  my $params = { @_, ua => HTTP::Tiny->new };
+  my $self = baptise $params, $pkg, qw//;
+  return $self;
+}
+
+sub _make_call {
+  my ($self, $when) = @_;
+  my $URL = sprintf "%s/%s", BASEURL, $when;
+  my $resp = HTTPTiny2h2o $self->ua->get($URL);
+  return $resp->content;
+}
+
+#TODO - add "list", date/MMDDYY, and novena/MMDDYY
+sub mp3Link {
+  my ($self, $when) = @_;
+  my @mp3 = qw/today yesterday tomorrow random/;
+  # invalid
+  return "" if (not grep { m/^$when$/i } @mp3);
+  # valid
+  return sprintf "%s/%s", DAILYURL, $self->_make_call($when)->get(0)->mp3Link;
+}
+
+sub day {
+  my ($self, $when) = @_;
+  my @days = qw/sunday monday tuesday wednesday thursday friday/;
+  return "" if (not grep { m/^$when$/i } @days);
+  return $self->_make_call($when)->get(0);
+}
+
+# Get detailed recitations
+sub Decades {
+  my ($self, $when) = @_;
+  my @mysteries = qw/joyful glorious sorrowful luminous/;
+  return "" if (not grep { m/^$when$/i } @mysteries);
+  return $self->_make_call($when);
 }
 
 123
@@ -26,7 +66,7 @@ Webservice::Rosary::API - Perl API client for the Rosary API at L<https://theros
 
 =head1 METHODS
 
-=head1 C<avemaria> UTILITY
+=head1 C<AveMaria> UTILITY
 
 =head1 ENVIRONMENT
 
--- t/01-basic.t
+++ t/01-basic.t
@@ -4,4 +4,8 @@ use Test::More;
 
 use_ok "Webservice::Rosary::API";
 
+my $Rosary = Webservice::Rosary::API->new;
+
+isa_ok $Rosary, "Webservice::Rosary::API";
+
 done_testing;
